830632a079df10f7424cd5eb9b03704b2cbd02ae
Lindsay, James <jlindsay@ds.dfci.harvard.edu>
2024-03-04 15:53:48 -0500

    added poetry fixed networkx upgrade

--- oncotreenx/__init__.py
+++ oncotreenx/__init__.py
@@ -1,3 +1,3 @@
 from __future__ import absolute_import
-from .oncotreenx import build_oncotree, get_basal, lookup_text
-__all__ = ["build_oncotree", "get_basal", "lookup_text"]
+from .oncotreenx import build_oncotree, get_basal, name_to_code
+__all__ = ["build_oncotree", "get_basal", "name_to_code"]
